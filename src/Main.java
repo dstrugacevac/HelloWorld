4ec888afccf4d9dc5c82537349e55f4f7d8186b8
David Strugacevac <dstrugacevac@layer.agency>
2024-10-23 18:33:32 +0200

    Prvi zadatak

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// Napiši program koji od korisnika traži unos niza brojeva i sprema ih u List-u.
+// Pronađi najmanji i najveći broj, zamijeni ih međusobno i ispiši novu List-u.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Treci sat predavanja.");
-        System.out.println("Cetvrti sat predavanja.");
-        System.out.println("Peti sat predavanja.");
+
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> brojevi = new ArrayList<>();
+
+        System.out.print("Unesite veličinu liste: ");
+        int velicinaListe = scanner.nextInt();
+
+        System.out.println("Unesite " + velicinaListe + " brojeva:");
+
+        for (int i = 0; i < velicinaListe; i++) {
+            brojevi.add(scanner.nextInt());
+        }
+
+        int min = brojevi.get(0);
+        int max = brojevi.get(0);
+
+        for (int i = 1; i < brojevi.size(); i++) {
+            if (brojevi.get(i) < min) {
+                min = brojevi.get(i);
+            }
+            if (brojevi.get(i) > max) {
+                max = brojevi.get(i);
+            }
+        }
+
+        Integer indexMinBroja = brojevi.indexOf(min);
+        Integer indexMaxBroja = brojevi.indexOf(max);
+
+        brojevi.set(indexMinBroja, max);
+        brojevi.set(indexMaxBroja, min);
+
+        System.out.println("Nova lista nakon zamjene najmanjeg i najvećeg broja:");
+        for (int broj : brojevi) {
+            System.out.print(broj + " ");
+        }
     }
 }
