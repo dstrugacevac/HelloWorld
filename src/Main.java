eabf6b1d9ffa0057d94e46532b50ff00978ad21e
David Strugacevac <dstrugacevac@layer.agency>
2024-10-24 08:08:03 +0200

    Zadatak za vjezbu

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,47 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// 2. Zadatak. Od korisnika trazite unos 10 brojeva te u listi pronadite koji broj se najvise puta ponavlja.
-// Za rezultat ispisite broj koji se najvise puta ponavljao u listi te koliko se puta pojavio u listi.
-// Treba uzeti u obriz i kada su svi brojevi razliciti.
+//Tražiti od korisnika da unese broj elemenata u listi te popuni elemente liste.
+//Nakon toga pretražiti po listi da li postoji ijedan duplikat, ukoliko postoji ispisi vrijednost true, ukoliko ne postoji ispisi vrijednost false.
 public class Main {
-
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> brojevi = new ArrayList<>();
+            Scanner scanner = new Scanner(System.in);
+            List<Integer> brojevi = new ArrayList<>();
 
-        System.out.println("Unesite 10 brojeva:");
-        for (int i = 0; i < 10; i++) {
-            brojevi.add(scanner.nextInt());
-        }
-        
-        int najcesciBroj = brojevi.get(0);
-        int maxPonavljanja = 1;
+            System.out.println("Unesite broj elemenata: ");
+            int brojElemenata = scanner.nextInt();
 
-        for (int i = 0; i < brojevi.size(); i++) {
-            int trenutniBroj = brojevi.get(i);
-            int brojPonavljanja = 0;
+            for (int i = 0; i < brojElemenata; i++) {
+                System.out.println("Unesite željeni broj: ");
+                brojevi.add(scanner.nextInt());
+            }
+            // 1, 6, 54, 23, 4
+            // 1, 6, 54, 23, 4
 
-            for (int j = 0; j < brojevi.size(); j++) {
-                if (brojevi.get(j) == trenutniBroj) {
-                    brojPonavljanja++;
+            Boolean duplikat = false;
+
+            for (int i = 0; i < brojevi.size(); i++){
+                int trenutniBroj = brojevi.get(i);
+
+                for (int j = 0; j < brojevi.size(); j++) {
+                    if (i != j) {
+                        if (brojevi.get(j) == trenutniBroj) {
+                        System.out.println("True");
+                        duplikat = true;
+                        break;
+                        }
+                    }
                 }
+
+                if (duplikat== true){
+                    break;
+                }
+
             }
 
-            if (brojPonavljanja >= maxPonavljanja) {
-                maxPonavljanja = brojPonavljanja;
-                najcesciBroj = trenutniBroj;
+            if (duplikat == false){
+                System.out.println("False");
             }
         }
-
-        if (maxPonavljanja==1){
-            System.out.println("Svi brojevi su razliciti.");
-        }else {
-            System.out.println("Broj koji se najviše puta ponavljao: " + najcesciBroj);
-            System.out.println("Ponavlja se " + maxPonavljanja + " puta.");
-        }
-    }
 }
