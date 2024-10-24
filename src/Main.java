49508a414f1998a81dc7f99153c9321a1c7de869
David Strugacevac <dstrugacevac@layer.agency>
2024-10-24 19:14:38 +0200

    Primjer funkcije oduzimanja

--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//Tražiti od korisnika da unese broj elemenata u listi te popuni elemente liste.
-//Nakon toga pretražiti po listi da li postoji ijedan duplikat, ukoliko postoji ispisi vrijednost true, ukoliko ne postoji ispisi vrijednost false.
 public class Main {
     public static void main(String[] args) {
 
             Scanner scanner = new Scanner(System.in);
-            List<Integer> brojevi = new ArrayList<>();
 
-            System.out.println("Unesite broj elemenata: ");
-            int brojElemenata = scanner.nextInt();
+            System.out.println("Unesite prvi broj: ");
+            int prviBroj = scanner.nextInt();
+            ispisiVrijednostElementa(prviBroj);
 
-            for (int i = 0; i < brojElemenata; i++) {
-                System.out.println("Unesite željeni broj: ");
-                brojevi.add(scanner.nextInt());
-            }
-            // 1, 6, 54, 23, 4
-            // 1, 6, 54, 23, 4
+            System.out.println("Unesite drugi broj: ");
+            int drugiBroj = scanner.nextInt();
+            ispisiVrijednostElementa(drugiBroj);
 
-            Boolean duplikat = false;
+            Integer rezultatOduzimanja = oduzmiBrojeve(prviBroj, drugiBroj);
 
-            for (int i = 0; i < brojevi.size(); i++){
-                int trenutniBroj = brojevi.get(i);
+            System.out.println("Rezultat je: " + rezultatOduzimanja);
+        }
 
-                for (int j = 0; j < brojevi.size(); j++) {
-                    if (i != j) {
-                        if (brojevi.get(j) == trenutniBroj) {
-                        System.out.println("True");
-                        duplikat = true;
-                        break;
-                        }
-                    }
-                }
+        public static void ispisiVrijednostElementa(Integer element){
+            // Integer element;
+            System.out.println("Vrijednost je: " + element);
+        }
 
-                if (duplikat== true){
-                    break;
-                }
-
-            }
-
-            if (duplikat == false){
-                System.out.println("False");
-            }
+        public static Integer oduzmiBrojeve(Integer element1, Integer element2){
+            Integer rezultat = element1 - element2;
+            return rezultat;
         }
 }
